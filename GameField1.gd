cc80d59ab9de446f2c1836c5dc7cc7c426d3997c
yoozakino <kazakove1800@gmail.com>
2025-10-08 14:45:19 +0300

    готовый макет до написания полноценной логики игры

--- GameField1.gd
+++ GameField1.gd
@@ -5,7 +5,10 @@ onready var NumLabel = $Number
 
 func _ready():
 	randomize()
+	NumLabel.modulate.a = 0
+	yield(get_tree().create_timer(3.0), "timeout")
 	_numgen()
+	_fade_in_numlabel()
 
 func _numgen():
 	var generated_num = randi() % 90 + 1
@@ -16,3 +19,10 @@ func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		MusicScene.get_node("MainSoundtrackPlayer").play()
 		get_tree().change_scene("res://MainScene.tscn")
+		
+func _fade_in_numlabel():
+	var tween = Tween.new()
+	add_child(tween)
+	tween.interpolate_property(NumLabel, "modulate:a", 0, 1, 1.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
+
